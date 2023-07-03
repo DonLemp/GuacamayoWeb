f5b91b49cd7bd4937b457ed9a5e9fec0b7e8fa66
DonLemp <eduardo_lemp@guacamayoconsultores.com>
2023-07-03 18:43:21 -0400

    Alfa02

---
+++ script_canvas.js
@@ -0,0 +1,29 @@
+const dibujo = document.getElementById("mi_canvas");
+const c = dibujo.getContext("2d");
+
+function draw(x, y) {
+
+    c.fillStyle = `rgb(
+        ${Math.floor(255 - x/3)},
+        ${Math.floor(255 - y*3)},
+        0)`;
+  if (isDrawing) {
+    c.beginPath();
+    c.arc(x, y, 5, 0, Math.PI * 2);
+    c.closePath();
+    c.fill();
+  }
+}
+
+dibujo.addEventListener("mousemove", (event) =>
+  draw(event.offsetX, event.offsetY)
+);
+dibujo.addEventListener("mousedown", () => (isDrawing = true));
+dibujo.addEventListener("mouseup", () => (isDrawing = false));
+
+document
+  .getElementById("mi_arte")
+  .addEventListener(
+    "click",
+    (event) => (event.target.href = dibujo.toDataURL())
+  );
