4c5ad4976df2504d83dca3ac1ae8157d1046a10d
DonLemp <eduardo_lemp@guacamayoconsultores.com>
2023-07-03 23:08:07 -0400

    Alfa02

--- dist/script.js
+++ dist/script.js
@@ -37,6 +37,10 @@
     // This will extract the region name from URL hash, and then load [region].html into the main #content div
     // var region = location.hash.toString() || '#first';
     // $('#content').load(region.slice(1) + '.html')
+
+    if($(window).mobileCheck()) {
+      $('#tabla_canvas').hide();
+    }
     
   });
   
